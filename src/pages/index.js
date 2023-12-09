92075d2621e87f83f78b892ade0a9dd5b3e524e8
Sofia Wheeler <sew9309@nyu.edu>
2023-12-09 16:48:35 -0500

    Completed midterm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 'use-client'
+import React, { useState } from 'react';
+import './page.css';
 
 const kanye_rest = `https://api.kanye.rest`;
 const cat_api = `https://api.thecatapi.com/v1/images/search?`;
@@ -24,40 +26,65 @@ export async function getStaticProps() {
   return {
     props: {
       quotesString,
-      imageUrls,
-      images
+      imageUrls
     },
   };
 }
 
-// async function getRandomImage() {
-//   const image = await cat_api.images.getRandomImage({
-//     hasBreeds: true,
-//   });
-//   return image;
-// }
-
-export default function Home ({ quotesString, imageUrls, images, image }) {
-  // console.log(quotes)
-  // console.log(quotesString);
-  // console.log(imageUrls);
-  console.log(images);
-
+  export default function Home({ quotesString, imageUrls }) {
+    // State to track the loading status
+    const [loading, setLoading] = useState(false);
+    const [newQuotesString, setNewQuotesString] = useState(quotesString);
+    const [newImageUrls, setNewImageUrls] = useState(imageUrls);
+  
+    async function makeApiCall() {
+      try {
+        // Set loading to true while making the API call
+        setLoading(true);
+  
+        // API call logic
+        const random_quote = await fetch(kanye_rest);
+        const quotes = await random_quote.json();
+        const updatedQuotesString = quotes.quote;
+  
+        const random_image = await fetch(cat_api, {
+          headers: {
+            'x-api-key': api_key,
+          },
+        });
+  
+        const images = await random_image.json();
+        const newImageUrls = images[0].url;
+  
+        // Update the state with the new data
+        setNewQuotesString(updatedQuotesString);
+        setNewImageUrls(newImageUrls);
+      } catch (error) {
+        console.error('Error refreshing API calls:', error);
+      } finally {
+        // Set loading back to false after the API call is complete
+        setLoading(false);
+      }
+    }
 
   return (
     <main>
-    <h1>Kats & Kanye</h1>
-    { quotesString }
-    <img id="kitty" src={imageUrls}></img>
-    {image}
+      <button id="refreshButton" onClick={makeApiCall} disabled={loading}>
+        {loading ? 'Refreshing...' : ''}
+      </button>
+
+    <h1 id='title'>Kats & Kanye</h1>
+    {newImageUrls && <img id="kitty" src={newImageUrls} alt="Cat" />}
+    {!newQuotesString && !newImageUrls && <p>Loading...</p>}
+    <div id="tweet">
+      <div class="userInfo">
+        <div class="profile" style={{backgroundImage: `url(${newImageUrls})`}}></div>
+        <h2 class="displayName">Kats & Kanye</h2>
+        <img class = "verification" src="https://cdn.worldvectorlogo.com/logos/twitter-verified-badge.svg" width="20px"></img>
+        <p class="username"><i>@katsandkanye</i></p>
+        </div>
+        {newQuotesString && <p class="tweetText">{newQuotesString}</p>}
+    </div>
+
     </main>
-  )
-
-    if (!quotesString, imageUrls) return null;
-
-    return (
-        <main>
-            <h1>Please work (this is null)</h1>
-        </main>
-    );
-}
+  )}
